d2f66b6f8b4ae3f01af66c1b3cc9e93f12cc8fc7
Albert <amuluowin@163.com>
2021-05-11 10:06:33 +0800

    update

--- src/Manager.php
+++ src/Manager.php
@@ -4,24 +4,70 @@ declare(strict_types=1);
 
 namespace Rabbit\Chrome\Headless;
 
-use HeadlessChromium\Browser\ProcessAwareBrowser;
+use HeadlessChromium\Browser;
 use HeadlessChromium\BrowserFactory;
+use Rabbit\Base\Contract\InitInterface;
+use Rabbit\Base\Exception\InvalidArgumentException;
+use Throwable;
 
-class Manager
+class Manager implements InitInterface
 {
     private BrowserFactory $factory;
 
-    protected array $pool = [];
+    private ?string $uri = null;
 
-    public function __construct(string $path = null)
+    protected array $options = [];
+
+    public function __construct(string $uri = null, array $options = [], string $path = null)
     {
         $this->factory = new BrowserFactory($path);
+        $this->uri = $uri;
+        $this->options = $options;
     }
 
-    public function getBrowser(string $name, array $options = []): ProcessAwareBrowser
+    public function init(): void
+    {
+        if ($this->uri) {
+            $arr = explode(',', $this->uri);
+            foreach ($arr as $uri) {
+                [$name, $uri] = explode('=', $uri);
+                $this->add($name, $uri, $this->options);
+            }
+        }
+    }
+
+    public function add(string $name, string $uri, array $options = []): Browser
+    {
+        if (isset($this->pool[$name])) {
+            throw new InvalidArgumentException("$name is exist!");
+        }
+        $this->pool[$name] = BrowserFactory::connectToBrowser($uri, $options);
+        return $this->pool[$name];
+    }
+
+    public function del(string $name): void
+    {
+        if (isset($this->pool[$name])) {
+            $this->pool[$name]->close();
+            unset($this->pool[$name]);
+        }
+    }
+
+    public function getBrowser(string $name, array $options = []): Browser
     {
         if (!isset($this->pool[$name])) {
             $this->pool[$name] = $this->factory->createBrowser($options);
+        } else {
+            $browser = $this->pool[$name];
+            $conn = $browser->getConnection();
+            if (!$conn->isConnected()) {
+                try {
+                    $conn->connect();
+                } catch (Throwable $e) {
+                    $browser->close();
+                    $this->pool[$name] = $this->factory->createBrowser($options);
+                }
+            }
         }
         return $this->pool[$name];
     }
